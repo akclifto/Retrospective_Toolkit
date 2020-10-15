bf36725e013e4c24aedfab53c39d6b87203ffffd
Christopher Gold <crgold@asu.edu>
2020-10-15 12:13:30 +0800

    finished login function for db service

--- server/service/auth.js
+++ server/service/auth.js
@@ -1,7 +1,21 @@
-function login(email, password) {
-    // lookup user by email
-    
+const userDAO = require('../dao/user');
+const bcrypt = require('bcrypt');
 
+async function login(email, password) {
+    // lookup user by email
+    try {
+        const user = await userDAO.findUserByEmail(email);
+
+        const match = await bcrypt.compare(password, user.pwhash);
+
+        if (match) {
+            return {id: user.id, roles: user.roles};
+        } else {
+            return Promise.reject('wrong username or password');
+        }
+    } catch(err) {
+        return Promise.reject('user not found');
+    }
 }
 
 module.exports = {login};
