994b31eb26741f25d9ebf943ca946be20f402d55
Christopher Gold <crgold@asu.edu>
2020-10-15 12:12:44 +0800

    updated var name

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,5 +1,5 @@
 function authenticate(req, res, next) {
-    if (!req.session || !req.session.clientId) {
+    if (!req.session || !req.session.user) {
         const err = new Error('You are not logged in');
         err.statusCode = 401;
         next(err);
