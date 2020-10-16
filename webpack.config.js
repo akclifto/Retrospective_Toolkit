f6945306382c7a87f165392bd21adeecf0e87a9c
akclifto <akclifto@asu.edu>
2020-10-16 12:28:30 -0700

    set up web env

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+// entry point (app.js) -> output final bundle file
+module.exports = {
+    mode : 'development',
+    entry: './src/main/ui/app.js',
+    output : {
+        path: path.join(__dirname, './src/main/resources/public'),
+        filename: 'bundle.js'
+    },
+    // loader, test rule using ' /.sometext$/ '  is called 'regular expression'
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        } ]
+    },
+    // devtool: 'eval-cheap-module-source-map',
+    // devServer: {
+    //     contentBase: path.join(__dirname, 'public'),
+    //     historyApiFallback: true
+    // }
+};
+
