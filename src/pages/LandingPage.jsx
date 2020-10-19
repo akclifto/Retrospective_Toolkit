19b9cbfe1c6ef67e37fec2c4d31ef3922a0eab57
Christopher Gold <crgold@asu.edu>
2020-10-19 14:08:41 +0800

    temporary landing page for testing

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function LandingPage() {
+
+    return (
+        <div>
+            <h1>Congrats! You made it!</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
