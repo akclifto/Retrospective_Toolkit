3c62a8d1e40511503297b81db36ead9301d6cbc5
Christopher Gold <crgold@asu.edu>
2020-10-20 13:55:27 +0800

    changed database connections for localhost testing

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,12 +1,12 @@
 const { Pool } = require('pg');
 
-const postgresClient = new Pool({
+/*const postgresClient = new Pool({
     connectionString: process.env.DATABASE_URL, 
     ssl: true,
     max: 20
-});
+});*/
 
-/* local connection settings
+// local connection settings
 const postgresClient = new Pool({
     host: 'localhost',
     port: 5432,
@@ -14,7 +14,7 @@ const postgresClient = new Pool({
     password: 'admin',
     database: 'postgres',
     max: 20
-});*/
+});
 
 //insure database is initialized
 
--- server/db/redis.js
+++ server/db/redis.js
@@ -1,14 +1,14 @@
 //var Redis = require('ioredis');
+const redis = require('redis');
 
 //Heroku deployment
-var redisClient = require('redis').createClient(process.env.REDIS_URL);
+//var redisClient = require('redis').createClient(process.env.REDIS_URL);
 //var redis = new Redis(process.env.REDIS_URL);
 
-//configure redis
-/* for local development
+//configure redis for local development
 const redisClient = redis.createClient({
     port: 6379,
     host: 'localhost'
-});*/
+});
 
 module.exports = redisClient;
