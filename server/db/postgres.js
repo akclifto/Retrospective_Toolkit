5f065e2e85331df7840f9517a472e2850c5d052a
Christopher Gold <crgold@asu.edu>
2020-10-20 17:21:53 +0800

    updated database connections for heroku deployment

--- server/db/postgres.js
+++ server/db/postgres.js
@@ -1,20 +1,20 @@
 const { Pool } = require('pg');
 
-/*const postgresClient = new Pool({
+const postgresClient = new Pool({
     connectionString: process.env.DATABASE_URL, 
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
     max: 20
-});*/
+});
 
 // local connection settings
-const postgresClient = new Pool({
+/*const postgresClient = new Pool({
     host: 'localhost',
     port: 5432,
     user: 'postgres',
     password: 'admin',
     database: 'postgres',
     max: 20
-});
+});*/
 
 //insure database is initialized
 
