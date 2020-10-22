6d7a904708383375c8e2baa0be1c86c8109be220
akclifto <akclifto@asu.edu>
2020-10-22 16:31:28 -0700

    merging expressPOC and reactSkeleton

--- server/db/redis.js
+++ server/db/redis.js
@@ -1,13 +1,15 @@
-//var Redis = require('ioredis');
-//const redis = require('redis');
+// var Redis = require('ioredis');
+const redis = require('redis');
 
-//Heroku deployment
-var redisClient = require('redis').createClient(process.env.REDIS_URL);
+/* Heroku deployment */
+// var redisClient = require('redis').createClient(process.env.REDIS_URL);
 
-//configure redis for local development
-/*const redisClient = redis.createClient({
-    port: 6379,
-    host: 'localhost'
-});*/
+/* configure redis for local development */
+let redisClient = redis.createClient();
+
+// const redisClient = redis.createClient({
+//     port: 6379,
+//     host: 'localhost'
+// });
 
 module.exports = redisClient;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ifAuth from '../components/ifAuth';
-import LandingPage from '../pages/LandingPage';
+import LandingPage from '../pages/LandingPage.jsx';
+import AuthLandingPage from '../pages/AuthLandingPage';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import { Switch, Route } from 'react-router-dom';
 
+
+//TODO: change AuthLandingPage route
 function Routes() {
     return (
         <Switch>
-                <Route exact path='/' component = {ifAuth(LandingPage)} />
+                <Route exact path='/' component = {LandingPage} />
+                <Route path='/login' component = {ifAuth(AuthLandingPage)} />
                 <Route path='/login' component = {Login} />
                 <Route path='/signup' component = {Signup} />
         </Switch>
