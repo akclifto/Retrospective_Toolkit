802eb284236fdb2a1406da377ff552225baf0c23
Christopher Gold <crgold@pop-os.localdomain>
2020-10-23 12:28:14 +0800

    integrated sergios landing page

---
+++ src/controller/Die.js
@@ -0,0 +1,71 @@
+/**
+ * Represents a die that makes a call to a server via a button.
+ */
+
+//Imports
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import classes from '../css/Die.module.css';
+import Emoji from '../components/Emoji'
+import Card from '@material-ui/core/Card';
+
+
+const Die = (props) => {
+    const assignedClasses = []; //Holds variable CSS classes to apply to a <p> tag
+    const [counter, setCounter] = useState(0); // Sets initial counter state to 0
+
+    //Will only occur when counter is changed. 
+    useEffect(() => {
+      //HTTP request
+      return () => {
+      alert('Rolling somewhere else...');
+      }
+    }, [counter]) //If [counter] was [] instead, useEffect would occur at first render.
+    
+
+    //Each if statement is called each render cycle. 
+    if (counter >= 1) {
+      assignedClasses.push(classes.red); //assignedClasses = ['bold', 'red']
+    }
+    if (counter >= 2) {
+      assignedClasses.push(classes.bold); //assignedClasses = ['bold']
+    }
+    if (counter >= 3) {
+      assignedClasses.push(classes.large)
+    }
+
+    //Increments counter by 1.
+    const incrementCounter = () => {
+      //setCounter(count + 1) doesn't update the value of count until after the render is done
+      setCounter(prevCounter => prevCounter +1)
+    }
+
+    //Returns JSX to DiceLanding
+    return (
+      <Card className={classes.Die}>
+        <Emoji label="dice" symbol="🎲"></Emoji>
+        <p 
+          className={assignedClasses.join(' ')}>
+            This die has {props.numSides} sides and is an {props.title} die. 
+          </p>
+        <button 
+          onClick={incrementCounter}
+          >
+          Roll This Die
+        </button>
+      </Card>
+    )
+}
+
+/**
+ * "Strict typing" of the properties that this function will need
+ * This mainly affects things like DiceLanding.js that pass in 
+ * title, numSides, and roll.
+ */
+Die.propTypes = {
+  title: PropTypes.string,
+  numSides: PropTypes.number,
+};
+
+export default Die;
+
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ifAuth from '../components/ifAuth';
-import LandingPage from '../pages/LandingPage';
+//import LandingPage from '../pages/LandingPage';
+import DiceLanding from '../pages/DiceLanding';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import { Switch, Route } from 'react-router-dom';
@@ -8,7 +9,7 @@ import { Switch, Route } from 'react-router-dom';
 function Routes() {
     return (
         <Switch>
-                <Route exact path='/' component = {ifAuth(LandingPage)} />
+                <Route exact path='/' component = {ifAuth(DiceLanding)} />
                 <Route path='/login' component = {Login} />
                 <Route path='/signup' component = {Signup} />
         </Switch>
