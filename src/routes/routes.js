a55a7ab8c08ce7fe6a58487fae56a88ed3429118
akclifto <akclifto@asu.edu>
2020-10-27 16:57:17 -0700

    add pageNotFound

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,18 +4,24 @@ import LandingPage from '../pages/LandingPage.jsx';
 import AuthLandingPage from '../pages/AuthLandingPage';
 import Login from '../pages/Login';
 import Signup from '../pages/Signup';
-import { Switch, Route } from 'react-router-dom';
+import PageNotFound from '../pages/PageNotFound';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 
 //TODO: change AuthLandingPage route
 function Routes() {
     return (
-        <Switch>
-                <Route exact path='/' component = {LandingPage} />
-                <Route path='/admin' component = {ifAuth(AuthLandingPage)} />
-                <Route path='/login' component = {Login} />
-                <Route path='/signup' component = {Signup} />
-        </Switch>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                        <Route exact path='/' component = {LandingPage} />
+                        <Route path='/admin' component = {ifAuth(AuthLandingPage)} />
+                        <Route path='/login' component = {Login} />
+                        <Route path='/signup' component = {Signup} />
+                        <Route component={} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
