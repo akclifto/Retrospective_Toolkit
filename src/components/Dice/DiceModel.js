e6ae0e1da5e8b5fad95bd32e806931af4210baec
Sergio P <sergprieto@live.com>
2020-10-28 13:06:29 -0700

    Button rolls multiple dice - WIP
    WIP because this branch currently merges the dice models and the container that holds the button.
    This is not an ideal solution.

--- src/components/Dice/DiceModel.js
+++ src/components/Dice/DiceModel.js
@@ -1,32 +1,37 @@
-import React, { useRef } from 'react';
-import ReactDice from 'react-dice-complete';
-import 'react-dice-complete/dist/react-dice-complete.css';
+// import React, { useRef } from 'react';
+// import ReactDice from 'react-dice-complete';
+// import 'react-dice-complete/dist/react-dice-complete.css';
+// import Button from '@material-ui/core/Button'
 
 
-const DiceModel = (props) => {
+// const DiceModel = (props) => {
 
-  let reactDice = useRef(null); //Needed once more dice get added
+//   let reactDice; //Needed once more dice get added
 
-  const rollAll = () => {
-    reactDice.rollAll()
-  }
+//   const rollAll = () => {
+//     reactDice.rollAll()
+//   }
 
-  const rollDoneCallback = (num) => {
-    props.result(num)
-  }
+//   const rollDoneCallback = (num) => {
+//     props.result(num)
+//   }
 
-    // Note: Default roll needs to be larger than the number of sides to not roll on page load
-    return (
-      <div>
-        <ReactDice
-          numDice={1}
-          defaultRoll={7}
-          rollDone={rollDoneCallback}
-          ref={dice => reactDice = dice}
-        />
-      </div>
-    )
+//     // Note: Default roll needs to be larger than the number of sides to not roll on page load
+//     return (
+//       <div>
+//         <ReactDice
+//           numDice={2}
+//           getDefault={-1}
+//           rollDone={rollDoneCallback}
+//           faceColor={"#EB0700"}
+//           dotColor={"#FFFFFF"}
+//           outline={true}
+//           ref={dice => reactDice = dice}
+//         />
+//         <Button onclick={rollAll}> button </Button>
+//       </div>
+//     )
 
-}
+// }
 
-export default DiceModel;
+// export default DiceModel;
--- src/components/Dice/Die.js
+++ src/components/Dice/Die.js
@@ -9,16 +9,23 @@ import classes from '../../styles/Die.module.css';
 import Card from '@material-ui/core/Card';
 import DiceModel from './DiceModel';
 import Button from '@material-ui/core/Button'
+import ReactDice from 'react-dice-complete';
+import 'react-dice-complete/dist/react-dice-complete.css';
 
 
 const Die = (props) => {
     const [diceResult, setDiceResult] = useState(0);
     const textRef = useRef("This die has " + props.numSides + " sides and is an " + props.title + " die.")
+    let reactDice = useRef(null); //Needed once more dice get added
 
     //Pass the reference to this function to DieModel.js
     const updateResult = (rollResult) => {
       setDiceResult(rollResult);
     };
+
+    const rollAll = () => {
+      reactDice.rollAll();
+    }
 
     if (diceResult !== 0) {
       textRef.current = "You rolled a " + diceResult + "!"; 
@@ -30,20 +37,29 @@ const Die = (props) => {
     // onClick={() => rollDice(props.numSides)} //TODO: implement the button to roll the dice
     return (
       <Card className={classes.Die}>
-        <DiceModel 
-        result={updateResult}
+        <ReactDice
+          numDice={2}
+          rollDone={updateResult}
+          disableIndividual={false}
+          defaultRoll={7}
+          faceColor={"#EB0700"}
+          dotColor={"#FFFFFF"}
+          outline={true}
+          ref={dice => reactDice = dice}
           />
         <p>
             {textRef.current}
           </p>
           <Button 
             variant="contained" 
+            onClick={rollAll}
             color="secondary">
               Roll Dice
             </Button>
       </Card>
     )
 }
+
 
 /**
  * "Strict typing" of the properties that this function will need
