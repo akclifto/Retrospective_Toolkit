c42c8201a82ed134506133a9729055d8138f14b2
Sergio P <sergprieto@live.com>
2020-10-31 11:20:42 -0700

    Add custom theme to root of project.
    Now all pages will automatically pull new primary/secondary colors.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import DiceLanding from '../containers/DiceLanding';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import Red from '@material-ui/core/colors/red';
 
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: Red[600]
-        },
-        secondary: {
-            main: '#3b3535'
-        }
-    }
-})
 
 const LandingPage = () => {
 
     return (
-        <ThemeProvider theme={theme}>
             <DiceLanding />
-        </ThemeProvider>
     );
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,20 +6,34 @@ import Login from '../pages/Login';
 import Signup from '../pages/Signup';
 import PageNotFound from '../pages/PageNotFound';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Red from '@material-ui/core/colors/red';
 
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: Red[600]
+        },
+        secondary: {
+            main: '#3b3535'
+        }
+    }
+})
 
 //TODO: change AuthLandingPage route
 function Routes() {
     return (
         <BrowserRouter>
             <div>
-                <Switch>
+                <ThemeProvider theme={theme}>
+                    <Switch>
                         <Route exact path='/' component = {LandingPage} />
                         <Route path='/admin' component = {ifAuth(AuthLandingPage)} />
                         <Route path='/login' component = {Login} />
                         <Route path='/signup' component = {Signup} />
                         <Route component = {PageNotFound} />
-                </Switch>
+                    </Switch>
+                </ThemeProvider>
             </div>
         </BrowserRouter>
     );
