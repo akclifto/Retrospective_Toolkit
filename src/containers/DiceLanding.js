190b10de94799c1e56636c6420b40ee28a4d1b59
Shane Thoney <sthoney@asu.edu>
2020-11-13 07:25:16 +0000

    Trying to use the NativeDice... stuck, but closer

--- src/containers/DiceLanding.js
+++ src/containers/DiceLanding.js
@@ -4,7 +4,7 @@
 
 //Imports
 import React from 'react';
-import Die from '../components/Dice/Die';
+import Die from '../components/DiceNative/Die';
 import {sides as sidesConst, themes as themeConst } from '../constants/DieConstants';
 import { makeStyles } from '@material-ui/core/styles';
 import InfoCard from '../components/InfoCard';
