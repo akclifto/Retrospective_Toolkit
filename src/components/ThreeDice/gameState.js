ba91ae65591e72c1618795a64c7959da75a476a0
crgold <crgold@asu.edu>
2020-12-01 03:29:41 +0800

    implement state management

---
+++ src/components/ThreeDice/gameState.js
@@ -0,0 +1,9 @@
+import { atom } from 'jotai';
+import { atomWithReset } from 'jotai/utils'
+
+export const gameStart = atom(false);
+export const gameReset = atom(false);
+
+export const diceDefault = atom([[-13, 5, 6], [-13, 6, 4], [-13, 7, 5], [-13, 5, 5], [-13, 6, 2]]);
+
+export const diceArray = atom([]);
