ee7ce70a3ce60dc83c1db2771e53c994b4c8d2bf
akclifto <akclifto@asu.edu>
2021-01-25 15:09:05 -0700

    update ds with destination icons, test

--- src/constants/IconsDataStructure.js
+++ src/constants/IconsDataStructure.js
@@ -1,3 +1,4 @@
+// Action Icons
 import altRoute from "../resources/action/alt_route-24px.svg";
 import brush from "../resources/action/brush-24px.svg";
 import directionsRun from "../resources/action/directions_run-24px.svg";
@@ -15,6 +16,25 @@ import sync from "../resources/action/sync-24px.svg";
 import toys from "../resources/action/toys-24px.svg";
 import watch from "../resources/action/watch-24px.svg";
 
+// Destination Icons
+import agriculture from "../resources/destination/agriculture-24px.svg";
+import apartment from "../resources/destination/apartment-24px.svg";
+import beachAccess from "../resources/destination/beach_access-24px.svg";
+import directionsBike from "../resources/destination/directions_bike-24px.svg";
+import directionsBoat from "../resources/destination/directions_boat-24px.svg";
+import directionsCar from "../resources/destination/directions_car-24px.svg";
+import flight from "../resources/destination/flight-24px.svg";
+import home from "../resources/destination/home-24px.svg";
+import hotel from "../resources/destination/hotel-24px.svg";
+import insertPhoto from "../resources/destination/insert_photo-24px.svg";
+import localCafe from "../resources/destination/local_cafe-24px.svg";
+import localFlorist from "../resources/destination/local_florist-24px.svg";
+import localShipping from "../resources/destination/local_shipping-24px.svg";
+import luggage from "../resources/destination/luggage-24px.svg";
+import naturePeople from "../resources/destination/nature_people-24px.svg";
+import pool from "../resources/destination/pool-24px.svg";
+
+// Export all icons
 export default [
   altRoute,
   brush,
@@ -32,4 +52,60 @@ export default [
   sync,
   toys,
   watch,
+
+  agriculture,
+  apartment,
+  beachAccess,
+  directionsBike,
+  directionsBoat,
+  directionsCar,
+  flight,
+  home,
+  hotel,
+  insertPhoto,
+  localCafe,
+  localFlorist,
+  localShipping,
+  luggage,
+  naturePeople,
+  pool,
+];
+
+// Export Action Icons only
+export const actionArr = [
+  altRoute,
+  brush,
+  directionsRun,
+  directionsWalk,
+  editBlack,
+  emojiObjects,
+  grass,
+  headset,
+  highlight,
+  musicNote,
+  removeRedEye,
+  security,
+  straighten,
+  sync,
+  toys,
+  watch,
+];
+
+export const destinationArr = [
+  agriculture,
+  apartment,
+  beachAccess,
+  directionsBike,
+  directionsBoat,
+  directionsCar,
+  flight,
+  home,
+  hotel,
+  insertPhoto,
+  localCafe,
+  localFlorist,
+  localShipping,
+  luggage,
+  naturePeople,
+  pool,
 ];
