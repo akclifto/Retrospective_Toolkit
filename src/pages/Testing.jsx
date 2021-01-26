7de10cc9d2a23428fe1ba3f7270b54d39b9d70b4
akclifto <akclifto@asu.edu>
2021-01-25 19:03:45 -0700

    lint issue

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import iconsArr from "../constants/IconsDataStructure";
 
 // iterate and renders array.
-const Testing = () => {
-  return (
-    <div>
-      <h1>Testing DS -- Total Items: {iconsArr.length} </h1>
-      {iconsArr.map((icon) => (
-        <div>
-          <li key={icon}>
-            {" "}
-            <img src={icon} alt={icon} /> Name: {icon}
-          </li>
-        </div>
-      ))}
-    </div>
-  );
-};
+const Testing = () => (
+  <div>
+    <h1>Testing DS -- Total Items: {iconsArr.length} </h1>
+    {iconsArr.map((icon) => (
+      <div>
+        <li key={icon}>
+          {" "}
+          <img src={icon} alt={icon} /> Name: {icon}
+        </li>
+      </div>
+    ))}
+  </div>
+);
 
 export default Testing;
