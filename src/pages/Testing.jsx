fdd9ef3bf52413d131dd225e2623d2812510a380
akclifto <akclifto@asu.edu>
2021-01-27 16:25:57 -0700

    Set up random selection in Testing, functional

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,13 +1,63 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid"; // clear annoying console warning about unique keys
 import iconsArr from "../constants/IconsDataStructure";
+
+function getIcons(finalIdx) {
+  const icons = [];
+  for (let i = 0; i < finalIdx.length; i += 1) {
+    const n = finalIdx[i];
+    icons.push(iconsArr[n]);
+  }
+  // eslint-disable-next-line no-console
+  // console.log(icons);
+  return icons;
+}
+
+// getRandom indexes based on number of die used. Avoid duplication.
+function getRandom(dieNumber) {
+  let rand;
+  const loopLength = iconsArr.length * 10;
+  const finalIdx = [];
+  let count = 0;
+
+  for (let i = 0; i < loopLength; i += 1) {
+    rand = Math.floor(Math.random() * iconsArr.length + 1);
+
+    if (count === dieNumber) {
+      // eslint-disable-next-line no-console
+      console.log(finalIdx);
+      break;
+    }
+
+    if (!finalIdx.includes(rand)) {
+      finalIdx.push(rand);
+      count += 1;
+    }
+  }
+  const ret = getIcons(finalIdx);
+  return ret;
+}
+
+// eslint-disable-next-line no-unused-vars
+const randomSelected = getRandom(36);
+// eslint-disable-next-line no-console
+// console.log(randomSelected);
 
 // iterate and renders array.
 const Testing = () => (
   <div>
+    <h2>Random Selected: </h2>
+    <div>
+      {randomSelected.map((icon) => (
+        <span>
+          <img src={icon} alt={icon} />{" "}
+        </span>
+      ))}
+    </div>
     <h1>Testing DS -- Total Items: {iconsArr.length} </h1>
     {iconsArr.map((icon) => (
       <div>
-        <li key={icon}>
+        <li key={uuidv4()}>
           {" "}
           <img src={icon} alt={icon} /> Name: {icon}
         </li>
