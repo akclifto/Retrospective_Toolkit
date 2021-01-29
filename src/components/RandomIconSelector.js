e6376c73f5235777b4ec00e7d84ec29a4c6e4a9b
akclifto <akclifto@asu.edu>
2021-01-29 16:22:40 -0700

    set up RandomIconSelector from Testing.jsx

---
+++ src/components/RandomIconSelector.js
@@ -0,0 +1,46 @@
+import iconsArr from "../constants/IconsDataStructure";
+
+/**
+ * Method to get icon information from iconsArr data structure.
+ * @param {*} finalIdx : index of randomly selected indeces to retrieve from iconsArr.
+ * @returns icons array.
+ */
+export function getIcons(finalIdx) {
+  const icons = [];
+  for (let i = 0; i < finalIdx.length; i += 1) {
+    const n = finalIdx[i];
+    icons.push(iconsArr[n]);
+  }
+  // eslint-disable-next-line no-console
+  // console.log(icons);
+  return icons;
+}
+
+/**
+ * Method compiles an array of randomly selected indeces, ensures no duplicated indeces.
+ * Calls getIcons to retrieve icon information from iconsArr.
+ * Currently hardcoded for 5 die, but can change to implicit representation easily.
+ * @param {*} dieNumber: number of die sides to index images.
+ * @returns final randomly selected icons array.
+ */
+export function getRandom(dieNumber) {
+  let rand;
+  const maxLen = dieNumber; // assuming this will eventually be (die * sides) = maxLen
+  const finalIdx = [];
+  let count = 0;
+
+  while (count < maxLen) {
+    rand = Math.floor(Math.random() * iconsArr.length);
+
+    if (!finalIdx.includes(rand)) {
+      finalIdx.push(rand);
+      count += 1;
+    }
+  }
+  // eslint-disable-next-line no-console
+  // console.log(finalIdx);
+  const ret = getIcons(finalIdx);
+  // eslint-disable-next-line no-console
+  // console.log(ret);
+  return ret;
+}
