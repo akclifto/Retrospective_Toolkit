6f17aa56a3063c18faf98d63c20b47d743594aa6
akclifto <akclifto@asu.edu>
2021-02-10 16:29:36 -0700

    auth base param test passing

--- server/tests/controller/auth.test.js
+++ server/tests/controller/auth.test.js
@@ -1,47 +1,62 @@
 const request = require("supertest");
 // const chai = require("chai");
 // eslint-disable-next-line no-unused-vars
-const server = require("../../index");
+const app = require("../../index");
+// const authController = require("../../controller/auth");
 
-const API = "http://localhost:5000";
+// const API = "http://localhost:5000";
+// const port = process.env.PORT || 5000;
+// let server;
+// let agent;
 
-// beforeAll(async () => {
-//   req();
+// beforeEach((done) => {
+//   // eslint-disable-next-line consistent-return
+//   server = app.listen(port, (err) => {
+//     if (err) return done(err);
+//     agent = request.agent(server);
+//     done();
+//   });
 // });
 
+afterEach((done) => app && app.close(done));
+
 describe("Controller/Auth Testing", () => {
+  // eslint-disable-next-line no-unused-vars
   const user = {
     email: "2134@at.com",
     password: "1234",
   };
 
-  it("post invalid login", async (done) => {
+  it("Send empty request parameter, shoud return status 400", async (done) => {
     try {
-      await request(API)
+      await request(app)
         .post("/api/users/login")
-        .set("Content-Type", "text/plain")
-        .send({ email: user.email })
-        .send({ password: user.password })
-        .expect(401);
+        .send({
+          email: "",
+          password: "",
+        })
+        .expect(400);
       done();
-    } catch (e) {
-      done(e);
+    } catch (err) {
+      done(err);
     }
   });
 
-  // it("Test invalid login endpoint", async (done) => {
+  // it("post invalid login", async (done) => {
   //   try {
-  //     const data = request(app).post("/api/users/login").send({
-  //       email: "123@a.com",
-  //       password: "1234",
-  //     });
-  //     expect.assertions(1);
-  //     await expect(data).toEqual(400);
+  //     // seed data
+  //     const data = await request(API)
+  //       .post("/api/users/login", authController.login)
+  //       .send({ email: user.email })
+  //       .send({ password: user.password });
+  //     // get response and test it
+  //     const response = await authController.login;
+  //     expect(response).toEqual(401);
+  //     expect(response).toEqual(data);
   //     done();
   //   } catch (e) {
   //     done(e);
   //   }
-  //   // expect(response.data.length).to.eql(30);
   // });
 });
 
@@ -53,3 +68,18 @@ afterAll(async (done) => {
     done(e);
   }
 });
+
+// it("Test invalid login endpoint", async (done) => {
+//   try {
+//     const data = request(app).post("/api/users/login").send({
+//       email: "123@a.com",
+//       password: "1234",
+//     });
+//     expect.assertions(1);
+//     await expect(data).toEqual(400);
+//     done();
+//   } catch (e) {
+//     done(e);
+//   }
+//   // expect(response.data.length).to.eql(30);
+// });
