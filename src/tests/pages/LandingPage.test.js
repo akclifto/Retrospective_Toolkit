b81aaa9e33ea980041c4503a91a76118bdd8857a
akclifto <akclifto@asu.edu>
2021-02-12 21:55:57 -0700

    add snapshot tests to pages/**, fix routes.test.js

--- src/tests/pages/LandingPage.test.js
+++ src/tests/pages/LandingPage.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import { shallow } from "enzyme";
 import LandingPage from "../../pages/LandingPage";
@@ -7,8 +6,8 @@ let wrapper;
 
 beforeAll(() => {
   // can't render the page bc of react-three/cannon module not found
-  const mockCannon = jest.genMockFromModule("@react-three/cannon");
-  jest.mock("@react-three/cannon", () => mockCannon);
+  // const mockCannon = jest.genMockFromModule("@react-three/cannon");
+  // jest.mock("@react-three/cannon", () => mockCannon);
 });
 
 beforeEach(() => {
