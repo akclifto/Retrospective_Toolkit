8d01b6a4471977a07fd29b6593e0129b8c805f64
akclifto <akclifto@asu.edu>
2021-02-16 21:42:52 -0700

    test constants/IconsDataStructure and Components/RandomIconSelector

---
+++ src/tests/components/RandomSelector.test.js
@@ -0,0 +1,19 @@
+import RandomIconSelector from "../../components/RandomIconSelector";
+import IconsArr from "../../constants/IconsDataStructure";
+
+describe("Components/RandomIconSelector Testing", () => {
+  it("Force duplication, should return empty array with log message.", () => {
+    const consoleSpy = jest.spyOn(console, "log");
+    const result = RandomIconSelector(49, IconsArr);
+    // const consoleLogSpy = console.log.mock.calls[0][0];
+    // console.log(consoleLogSpy);
+    expect(consoleSpy).toHaveBeenCalled();
+    expect(result).toEqual([]);
+  });
+
+  it("Test RandomIconSelector, should return valid array of length 12.", () => {
+    const result = RandomIconSelector(12, IconsArr);
+    expect(result).not.toBeNull();
+    expect(result.length).toBe(12);
+  });
+});
