b96917941081be4f2dc4d79e3d59b513957a8449
crgold <crgold@asu.edu>
2021-02-17 14:46:01 +0800

    tweak collision mesh to fit new table better

--- src/components/ThreeDice/ThreeDice.js
+++ src/components/ThreeDice/ThreeDice.js
@@ -23,27 +23,27 @@ import {
 
 const CollisionMesh = () => {
   const [floor] = useBox(() => ({
-    position: [0.5, 0, 0],
+    position: [0, 0, 0],
     rotation: [0, -Math.PI / 2, 0],
     args: [14, 0, 22],
     material: { friction: 1000 },
   }));
   const [left] = useBox(() => ({
-    position: [-10.5, 1, 0],
+    position: [-11.1, 1, 0],
     rotation: [0, -Math.PI / 2, 0],
     args: [13, 4, 0],
   }));
   const [right] = useBox(() => ({
-    position: [11.3, 1, 0],
+    position: [11.1, 1, 0],
     rotation: [0, -Math.PI / 2, 0],
     args: [13, 4, 0],
   }));
   const [top] = useBox(() => ({
-    position: [0.5, 1, 6.5],
+    position: [0, 1, 6.75],
     args: [22, 4, 0],
   }));
   const [bottom] = useBox(() => ({
-    position: [0.5, 1, -6.5],
+    position: [0, 1, -6.75],
     args: [22, 4, 0],
   }));
   return (
@@ -53,19 +53,19 @@ const CollisionMesh = () => {
         <meshStandardMaterial color="red" transparent opacity={0} />
       </mesh>
       <mesh ref={left}>
-        <boxBufferGeometry args={[13, 4, 0]} />
+        <boxBufferGeometry args={[13.5, 2, 0]} />
         <meshStandardMaterial color="red" transparent opacity={0} />
       </mesh>
       <mesh ref={right}>
-        <boxBufferGeometry args={[13, 4, 0]} />
+        <boxBufferGeometry args={[13.5, 2, 0]} />
         <meshStandardMaterial color="red" transparent opacity={0} />
       </mesh>
       <mesh ref={top}>
-        <boxBufferGeometry args={[22, 4, 0]} />
+        <boxBufferGeometry args={[22.2, 2, 0]} />
         <meshStandardMaterial color="red" transparent opacity={0} />
       </mesh>
       <mesh ref={bottom}>
-        <boxBufferGeometry args={[22, 4, 0]} />
+        <boxBufferGeometry args={[22.2, 2, 0]} />
         <meshStandardMaterial color="red" transparent opacity={0} />
       </mesh>
     </group>
