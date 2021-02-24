2e26de8ccb590b07705a7627b497a88a2ef02dce
Sergio Prieto <sergprieto@live.com>
2021-02-24 15:55:40 -0800

    Update snapshot test to ThreeDice to showcase both errors

--- src/tests/components/ThreeDice.test.js
+++ src/tests/components/ThreeDice.test.js
@@ -1,5 +1,7 @@
+/* eslint no-unused-vars: 0 */
 import React from "react";
 import { shallow } from "enzyme";
+import TestRenderer from "react-test-renderer";
 import ThreeDice from "../../components/ThreeDice/ThreeDice";
 
 let wrapper;
@@ -10,4 +12,5 @@ beforeAll(() => {
 
 test("SNAPSHOT TEST THREEDICE", () => {
   expect(wrapper).toMatchSnapshot();
+  const testRenderer = TestRenderer.create(<ThreeDice />);
 });
