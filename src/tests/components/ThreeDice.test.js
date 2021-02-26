86fafa48681f6cc577e13ac62f9ab62e9f86daaf
Sergio Prieto <sergprieto@live.com>
2021-02-26 06:45:13 -0800

    Add tests to find html/button code

--- src/tests/components/ThreeDice.test.js
+++ src/tests/components/ThreeDice.test.js
@@ -1,16 +1,32 @@
 /* eslint no-unused-vars: 0 */
 import React from "react";
+import renderer from "react-test-renderer";
+import { install } from "resize-observer";
 import { shallow } from "enzyme";
-import TestRenderer from "react-test-renderer";
+import Button from "@material-ui/core/Button";
+import {
+  render,
+  getByText,
+  getByLabelText,
+  screen,
+} from "@testing-library/react";
 import ThreeDice from "../../components/ThreeDice/ThreeDice";
 
-let wrapper;
-
 beforeAll(() => {
-  wrapper = shallow(<ThreeDice />);
+  if (!window.ResizeObserver) install();
+  jest.spyOn(console, "warn").mockImplementation(() => {});
+  jest.spyOn(console, "error").mockImplementation(() => {});
+  // jest.spyOn(console, "log").mockImplementation(() => {});
 });
 
 test("SNAPSHOT TEST THREEDICE", () => {
-  expect(wrapper).toMatchSnapshot();
-  const testRenderer = TestRenderer.create(<ThreeDice />);
+  const testRenderer = renderer.create(<ThreeDice />);
+
+  expect(testRenderer.toTree()).toMatchSnapshot();
+});
+
+test("Custom Button and Loading Html elements are loading", () => {
+  expect(screen.getByText("roll")).toBe(true);
+  expect(getByText.contains("Button")).toBe(true);
+  expect(getByText.contains("Html")).toBe(true);
 });
