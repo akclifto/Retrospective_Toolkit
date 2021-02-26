3e86e0d3477945ab045f9637297b8abc7f4577d4
crgold <crgold@asu.edu>
2021-02-26 20:35:55 +0800

    fix ThreeDice test

--- src/tests/components/ThreeDice.test.js
+++ src/tests/components/ThreeDice.test.js
@@ -2,6 +2,8 @@ import React from "react";
 import { shallow } from "enzyme";
 import ThreeDice from "../../components/ThreeDice/ThreeDice";
 
-it("renders and matches snapshot", () => {
-  expect(shallow(<ThreeDice />)).toMatchSnapshot();
+describe("rendering components", () => {
+  it("renders and matches snapshot", () => {
+    expect(shallow(<ThreeDice />)).toMatchSnapshot();
+  });
 });
