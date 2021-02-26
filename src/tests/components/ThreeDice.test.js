82af1fe1d1ebeac7f2079b827a73d85977cef346
crgold <crgold@asu.edu>
2021-02-26 20:09:52 +0800

    snapshot test for ThreeDice.js

--- src/tests/components/ThreeDice.test.js
+++ src/tests/components/ThreeDice.test.js
@@ -1,10 +1,7 @@
-/* eslint no-unused-vars: 0 */
 import React from "react";
 import { shallow } from "enzyme";
 import ThreeDice from "../../components/ThreeDice/ThreeDice";
 
-describe("rendering components", () => {
-  it("renders ThreeDice without crashing", () => {
-    shallow(<ThreeDice />);
-  });
+it("renders and matches snapshot", () => {
+  expect(shallow(<ThreeDice />)).toMatchSnapshot();
 });
