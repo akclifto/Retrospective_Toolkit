13d0ac93bfe7400b75023c2946b6c69ae3519b35
crgold <crgold@asu.edu>
2021-02-26 12:01:36 +0800

    refactor to modularize code and ease testing

---
+++ src/components/ThreeDice/CollisionMesh.js
@@ -0,0 +1,56 @@
+import React from "react";
+// eslint-disable-next-line import/no-unresolved
+import { useBox } from "@react-three/cannon";
+
+const CollisionMesh = () => {
+  const [floor] = useBox(() => ({
+    position: [0, 0, 0],
+    rotation: [0, -Math.PI / 2, 0],
+    args: [14, 0, 22],
+    material: { friction: 1000 },
+  }));
+  const [left] = useBox(() => ({
+    position: [-11.1, 1, 0],
+    rotation: [0, -Math.PI / 2, 0],
+    args: [13, 4, 0],
+  }));
+  const [right] = useBox(() => ({
+    position: [11.1, 1, 0],
+    rotation: [0, -Math.PI / 2, 0],
+    args: [13, 4, 0],
+  }));
+  const [top] = useBox(() => ({
+    position: [0, 1, 6.75],
+    args: [22, 4, 0],
+  }));
+  const [bottom] = useBox(() => ({
+    position: [0, 1, -6.75],
+    args: [22, 4, 0],
+  }));
+  return (
+    <group>
+      <mesh ref={floor}>
+        <boxBufferGeometry args={[14, 0, 22]} />
+        <meshStandardMaterial color="red" transparent opacity={0} />
+      </mesh>
+      <mesh ref={left}>
+        <boxBufferGeometry args={[13.5, 2, 0]} />
+        <meshStandardMaterial color="red" transparent opacity={0} />
+      </mesh>
+      <mesh ref={right}>
+        <boxBufferGeometry args={[13.5, 2, 0]} />
+        <meshStandardMaterial color="red" transparent opacity={0} />
+      </mesh>
+      <mesh ref={top}>
+        <boxBufferGeometry args={[22.2, 2, 0]} />
+        <meshStandardMaterial color="red" transparent opacity={0} />
+      </mesh>
+      <mesh ref={bottom}>
+        <boxBufferGeometry args={[22.2, 2, 0]} />
+        <meshStandardMaterial color="red" transparent opacity={0} />
+      </mesh>
+    </group>
+  );
+};
+
+export default CollisionMesh;
---
+++ src/components/ThreeDice/ThemedDie.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from "react";
+import { useTexture } from "@react-three/drei";
+// eslint-disable-next-line import/no-unresolved
+import { useBox } from "@react-three/cannon";
+import PropTypes from "prop-types";
+import { randomDiceImages as themes } from "../Dice/Dice";
+
+// let textures = [];
+const ThemedDie = (props) => {
+  const { theme, dicePos, rerollToggle } = props;
+  const textures = useTexture([...themes]);
+
+  const [mesh, api] = useBox(() => ({
+    mass: 300,
+    inertia: 13,
+    rotation: [
+      Math.random() * Math.PI,
+      Math.random() * Math.PI,
+      Math.random() * Math.PI,
+    ],
+    linearDamping: 0.5,
+    angularDamping: 0.1,
+    material: { restitution: 0.3 },
+  }));
+
+  useEffect(() => {
+    api.position.set(dicePos[0], dicePos[1], dicePos[2]);
+    api.velocity.set(15, 0, -10);
+    api.angularVelocity.set(-15, 2, -10);
+  }, [api.angularVelocity, api.position, api.velocity, dicePos, rerollToggle]);
+
+  if (theme === "random") {
+    return (
+      <mesh
+        onClick={() => {
+          api.position.set(dicePos[0], dicePos[1], dicePos[2]);
+          api.velocity.set(15, 0, -10);
+          api.angularVelocity.set(-15, 2, -10);
+        }}
+        ref={mesh}
+      >
+        <boxBufferGeometry />
+        {textures.map((image) => (
+          <meshStandardMaterial
+            key={image.uuid}
+            flatShading
+            roughness={0.8}
+            attachArray="material"
+            map={image}
+          />
+        ))}
+      </mesh>
+    );
+  }
+
+  // if we are here, something has gone wrong
+  return new Error("problem encountered in ThemedDice");
+};
+
+ThemedDie.propTypes = {
+  theme: PropTypes.string.isRequired,
+  dicePos: PropTypes.arrayOf(PropTypes.number).isRequired,
+  rerollToggle: PropTypes.bool.isRequired,
+};
+
+export default ThemedDie;
