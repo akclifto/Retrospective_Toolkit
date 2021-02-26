e1dfb12acf9953a733a055bc69baac77c66a10e5
crgold <crgold@asu.edu>
2021-02-26 12:45:40 +0800

    render test for ThreeDice

--- src/tests/components/ThreeDice.test.js
+++ src/tests/components/ThreeDice.test.js
@@ -1,16 +1,10 @@
 /* eslint no-unused-vars: 0 */
 import React from "react";
 import { shallow } from "enzyme";
-import TestRenderer from "react-test-renderer";
 import ThreeDice from "../../components/ThreeDice/ThreeDice";
 
-let wrapper;
-
-beforeAll(() => {
-  wrapper = shallow(<ThreeDice />);
-});
-
-test("SNAPSHOT TEST THREEDICE", () => {
-  expect(wrapper).toMatchSnapshot();
-  const testRenderer = TestRenderer.create(<ThreeDice />);
+describe("rendering components", () => {
+  it("renders ThreeDice without crashing", () => {
+    shallow(<ThreeDice />);
+  });
 });
