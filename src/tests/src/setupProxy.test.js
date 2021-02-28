e1d89eea1c1ddd4ed2f4aa8dccd7160c4eb7c2a6
akclifto <akclifto@asu.edu>
2021-02-27 17:12:54 -0700

    restructure tests/src, adjust setupProxyTest

--- src/tests/src/setupProxy.test.js
+++ src/tests/src/setupProxy.test.js
@@ -1,15 +1,18 @@
 /* eslint-disable no-console */
 // eslint-disable-next-line import/no-extraneous-dependencies
-const express = require("./__mocks__/express");
+const request = require("supertest");
+const express = require("../__mocks__/express");
 const proxy = require("../../setupProxy");
 
-const app = express();
+const appMock = express();
 
 describe("SetupProxy Tests", () => {
   it("Test proxy creation", (done) => {
-    proxy(app);
-    expect(proxy.length).toBeGreaterThanOrEqual(1);
+    const result = request(proxy(appMock));
     expect(proxy).toBeDefined();
+    expect(proxy.length).toBeGreaterThanOrEqual(1);
+
+    expect(result).toBeDefined();
     done();
   });
 });
