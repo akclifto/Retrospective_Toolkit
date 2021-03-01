c9e88b47cbb30930fc8e8f503229d0b2e608041e
crgold <crgold@asu.edu>
2021-03-01 23:17:35 +0800

    GameManager testing

---
+++ src/tests/components/GameManager.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { Provider } from "jotai";
+
+import GameManager from "../../components/ThreeDice/GameManager";
+
+it("should do a snapshot test on ThemedDie", async () => {
+  const wrapper = shallow(
+    <Provider>
+      <GameManager />
+    </Provider>
+  ).dive();
+  expect(wrapper).toMatchSnapshot();
+});
