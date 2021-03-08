26c213a6681b90243b33842b38269529b8e35bab
akclifto <akclifto@asu.edu>
2021-03-08 12:41:45 -0700

    restructure, makestyles cause too many issues, convert to css

--- src/components/infoCard_test.js
+++ src/components/infoCard_test.js
@@ -1,51 +1,19 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
+import "../styles/InfoCard.css";
 
-const useStyles = makeStyles({
-  root: {
-    width: "90%",
-    height: "88%",
-    maxHeight: "400px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "left",
-    margin: "10px 0px 0px 10px", // top right bottom left
-    boxShadow: "0 1px 3px rgba(0,0,0,0.12)",
-    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
-    "&:hover": {
-      boxShadow: "0 10px 13px rgba(0,0,0,0.25)",
-    },
-    titleButton: {
-      textDecoration: "none",
-      width: "50%",
-    },
-  },
-});
-
-function InfoCard(props) {
-  const classes = useStyles();
-  const { title, body, body2, body3 } = props;
+function SetInfoCard(props) {
+  // const classes = useStyles();
+  const { body, body2, body3 } = props;
 
   return (
-    <Card className={classes.root} variant="outlined">
+    <Card className="infoCard--root" variant="outlined">
       <CardContent>
-        <Typography
-          variant="h4"
-          className={classes.title}
-          color="textPrimary"
-          gutterBottom
-          align="center"
-        >
-          <div className={classes.root}>
-            {title}
-            Cube Game
-          </div>
-        </Typography>
         <div>
           <Typography variant="body1" component="p">
             {body}
@@ -67,30 +35,43 @@ function InfoCard(props) {
   );
 }
 
-class infoCardTest extends React.Component {
+class InfoCardTest extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showGameInfo: true };
+    this.state = { showActivityInfo: true };
     // eslint rules force og constructor, bind the functions
     this.clickHandler = this.clickHandler.bind(this);
   }
 
   clickHandler() {
-    // eslint-disable-next-line no-undef
-    this.setState((state) => ({ showGameInfo: !state.showGameInfo }));
+    this.setState((state) => ({ showActivityInfo: !state.showActivityInfo }));
   }
 
   render() {
-    const { showGameInfo } = this.state;
+    const { showActivityInfo } = this.state;
+    const { title } = this.props;
 
     return (
       <div>
-        <button className="root" type="button" onClick={this.clickHandler}>
-          Cube Game
+        <button
+          className="titleButton"
+          type="button"
+          onClick={this.clickHandler}
+        >
+          <Typography
+            variant="h4"
+            // className={classes.title}
+            color="textPrimary"
+            gutterBottom
+            align="center"
+          >
+            {title}
+            Title
+          </Typography>
         </button>
-        {showGameInfo ? (
+        {showActivityInfo ? (
           <div>
-            <InfoCard onClick={this.clickHandler} />
+            <SetInfoCard />
           </div>
         ) : null}
       </div>
@@ -98,11 +79,15 @@ class infoCardTest extends React.Component {
   }
 }
 
-InfoCard.propTypes = {
-  title: PropTypes.string.isRequired,
+SetInfoCard.propTypes = {
+  // title: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   body2: PropTypes.string.isRequired,
   body3: PropTypes.string.isRequired,
 };
 
-export default infoCardTest;
+InfoCardTest.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default InfoCardTest;
