ff2aa8cd0d0cfc03d39b80647da19a85a807d69f
akclifto <akclifto@asu.edu>
2021-03-08 16:31:04 -0700

    update tests

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,8 +11,8 @@ import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import PageNotFound from "../pages/PageNotFound";
 // can remove this after testing IconsDataStructure is done
-// import Testing from "../pages/Testing";
-import InfoCard from "../components/InfoCard";
+import Testing from "../pages/Testing";
+// import InfoCard from "../components/InfoCard";
 
 const theme = createMuiTheme({
   palette: {
@@ -35,7 +35,7 @@ function Routes() {
             <Route path="/admin" component={ifAuth(AuthLandingPage)} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-            <Route path="/testing" component={InfoCard} />
+            <Route path="/testing" component={Testing} />
             <Route component={PageNotFound} />
           </Switch>
         </ThemeProvider>
