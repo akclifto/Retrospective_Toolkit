c5db93b4ee987efff8bb7fd32260df4531adc343
crgold <crgold@asu.edu>
2021-04-07 14:56:16 +0800

    WIP - component to control socket dynamics

---
+++ src/components/ThreeDice/NetworkManager.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import * as io from "socket.io-client";
+
+const generateRoll = () => {
+  // some logic here
+};
+
+const sendRoll = () => {
+  // some logic here
+};
+
+const getRoll = () => {
+  // some logic here
+};
+
+const NetworkManager = () => {
+  const [socket, setSocket] = useState();
+  let roomId;
+  console.log("rendering");
+
+  if (sessionStorage.getItem("role") === null) {
+    roomId = uuidv4();
+    console.log("generate id");
+    // if the role is null then we will create a new room and assign the room Id to session storage
+    // sessionStorage.setItem("role", "host");
+    // console.log(window.location.port);
+    // console.log(window.location.origin);
+    // console.log(window.location.pathname.split("/").pop());
+  } else {
+    // get roomId and redirect back to room page
+    sessionStorage.getItem("roomId");
+  }
+
+  useEffect(() => {
+    setSocket(
+      io.connect(window.location.origin.replace(window.location.port, "5000"))
+    );
+  }, []);
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (socket) {
+      console.log("on fired");
+      socket.emit("board:create", roomId);
+      console.log("emit fired");
+      socket.on("create:complete", (msg) => {
+        console.log(msg);
+        window.location.assign(`/board/${roomId}`);
+      });
+    }
+  }, [roomId, socket]);
+
+  return null;
+};
+
+export default NetworkManager;
+export { generateRoll, sendRoll, getRoll };
