6ef13c6324d9847ec7724ac74a4b38dc1b0c7af3
crgold <crgold@asu.edu>
2021-04-18 13:50:03 +0800

    added production env var for redis url

--- server/db/redis.js
+++ server/db/redis.js
@@ -1,10 +1,12 @@
 // var Redis = require('ioredis');
 // const redis = require('redis');
 
+const redisURL =
+  process.env.REDIS_URL ||
+  "redis://h:p343f46b6946ec0516ef9ace243e856e99223c708ff817633212d183ae46674f6@ec2-52-71-199-175.compute-1.amazonaws.com:17059";
+
 // Heroku deployment
-const redisClient = require("redis").createClient(
-  "redis://h:p343f46b6946ec0516ef9ace243e856e99223c708ff817633212d183ae46674f6@ec2-52-71-199-175.compute-1.amazonaws.com:17059"
-);
+const redisClient = require("redis").createClient(redisURL);
 /* configure redis for local development */
 // const redisClient = redis.createClient({
 //     port: 6379,
