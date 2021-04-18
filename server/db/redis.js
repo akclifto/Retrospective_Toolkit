c2e6971391f89b74220b9251dbcc8b3d07db8243
crgold <crgold@asu.edu>
2021-04-18 12:39:14 +0800

    update redis url

--- server/db/redis.js
+++ server/db/redis.js
@@ -3,9 +3,8 @@
 
 // Heroku deployment
 const redisClient = require("redis").createClient(
-  "redis://h:p343f46b6946ec0516ef9ace243e856e99223c708ff817633212d183ae46674f6@ec2-3-210-163-2.compute-1.amazonaws.com:19299"
+  "redis://h:p343f46b6946ec0516ef9ace243e856e99223c708ff817633212d183ae46674f6@ec2-52-71-199-175.compute-1.amazonaws.com:17059"
 );
-
 /* configure redis for local development */
 // const redisClient = redis.createClient({
 //     port: 6379,
